2b6db4283f63432a43b4dbe901edc27a9c988744
Matthew Rapati <mattrapati@gmail.com>
2018-01-21 19:01:30 -0500

    Wayfinding, input and movement, pixel shader

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 5f;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start() {
+        Invoke("LoadLevel", levelLoadDelay);
+    }
+
+    void Update() {
+
+    }
+
+    private void LoadLevel() {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 8f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 8f;
+    [Tooltip("In m")] [SerializeField] float xLimit = 5f;
+    [Tooltip("In m")] [SerializeField] float yLimit = 3f;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float clampedXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -Mathf.Abs(xLimit), xLimit);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float clampedYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -Mathf.Abs(yLimit), yLimit);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+	}
+}

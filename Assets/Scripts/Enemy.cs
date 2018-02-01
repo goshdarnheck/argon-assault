23416bbe3ab6438331ec3e3c17bd444692cca2c0
Matthew Rapati <mattrapati@gmail.com>
2018-02-01 15:37:02 -0500

    Enemy FXs added to "spawned at runtime" object and destroy themselves after 3 seconds

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
 
 	// Use this for initialization
 	void Start () {
@@ -26,10 +27,8 @@ public class Enemy : MonoBehaviour {
     }
 
     private void OnParticleCollision(GameObject other) {
-        // print("Particles collied with enemy" + gameObject.name);
-
-        Instantiate(deathFX, transform.position, Quaternion.identity);
-
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
